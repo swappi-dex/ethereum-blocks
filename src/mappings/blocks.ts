cc6d968d9ab549d91767b6a7c6e14ffa0e382b1d
PanaW <pana.wang@outlook.com>
2022-11-18 10:02:37 +0800

    comment useless block fields

--- src/mappings/blocks.ts
+++ src/mappings/blocks.ts
@@ -11,16 +11,16 @@ export function handleBlock(block: EthereumBlock): void {
     let blockEntity = new Block(id);
     blockEntity.number = block.number;
     blockEntity.timestamp = block.timestamp;
-    blockEntity.parentHash = block.parentHash.toHex();
-    blockEntity.author = block.author.toHex();
-    blockEntity.difficulty = block.difficulty;
-    blockEntity.totalDifficulty = block.totalDifficulty;
-    blockEntity.gasUsed = block.gasUsed;
-    blockEntity.gasLimit = block.gasLimit;
-    blockEntity.receiptsRoot = block.receiptsRoot.toHex();
-    blockEntity.transactionsRoot = block.transactionsRoot.toHex();
-    blockEntity.stateRoot = block.stateRoot.toHex();
-    blockEntity.size = block.size;
-    blockEntity.unclesHash = block.unclesHash.toHex();
+    // blockEntity.parentHash = block.parentHash.toHex();
+    // blockEntity.author = block.author.toHex();
+    // blockEntity.difficulty = block.difficulty;
+    // blockEntity.totalDifficulty = block.totalDifficulty;
+    // blockEntity.gasUsed = block.gasUsed;
+    // blockEntity.gasLimit = block.gasLimit;
+    // blockEntity.receiptsRoot = block.receiptsRoot.toHex();
+    // blockEntity.transactionsRoot = block.transactionsRoot.toHex();
+    // blockEntity.stateRoot = block.stateRoot.toHex();
+    // blockEntity.size = block.size;
+    // blockEntity.unclesHash = block.unclesHash.toHex();
     blockEntity.save();
   }
